3d22ec91a52ea5c84d5af232ae7b1c9d4865d156
Ankur Work <ankurwork@ankurs-mbp.lan>
2022-11-06 23:00:06 -0700

    testing azure connactivity

--- environments/stage/providers.tf
+++ environments/stage/providers.tf
@@ -1,22 +1,22 @@
-# variable "subscription_id" {
-#   type      = string
-#   sensitive = true
-# }
+variable "subscription_id" {
+  type      = string
+  sensitive = true
+}
 
-# variable "client_id" {
-#   type      = string
-#   sensitive = true
-# }
+variable "client_id" {
+  type      = string
+  sensitive = true
+}
 
-# variable "client_secret" {
-#   type      = string
-#   sensitive = true
-# }
+variable "client_secret" {
+  type      = string
+  sensitive = true
+}
 
-# variable "tenant_id" {
-#   type      = string
-#   sensitive = true
-# }
+variable "tenant_id" {
+  type      = string
+  sensitive = true
+}
 
 terraform {
   required_providers {
