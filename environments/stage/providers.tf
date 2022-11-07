3861fadee4813f444ac2f2e6f215d42edad2279d
Ankur Work <ankurwork@ankurs-mbp.lan>
2022-11-06 23:05:17 -0700

    testing azure connactivity

--- environments/stage/providers.tf
+++ environments/stage/providers.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
+  subscription_id = var.TF_VAR_subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
